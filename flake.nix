bcdca466541d2193dbf90bb21756c6abb6d97247
jevin <jevin@quickjack.ca>
2022-04-01 08:23:03 -0400

    Using unstable now

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     duplicity_script.url = "/home/jevin/code/personal/duplicity_script";
   };
