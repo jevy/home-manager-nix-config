3aa2d5ab419022c0c537efc425ffa44bb52840fd
jevin <jevin@quickjack.ca>
2022-05-06 20:04:16 -0400

    Mailcap

--- home.nix
+++ home.nix
@@ -20,7 +20,6 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # programs.home-manager.useGlobalPkgs = true;
 
   # programs.git = {
   #   enable = true;
