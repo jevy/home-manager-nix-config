108efa96a9691a40cff76713e1ab3cb67de923bc
jevin <jevin@quickjack.ca>
2022-06-07 06:55:34 -0400

    Yummy neovim remote

--- cli-linux.nix
+++ cli-linux.nix
@@ -18,5 +18,10 @@
       "shift+page_up" = "scroll_page_up";
       "shift+page_down" = "scroll_page_down";
     };
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      visual_bell_duration = "0.1";
+    };
   };
 }
