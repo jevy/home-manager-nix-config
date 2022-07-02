643463e28df1ed51bd0262059abf9113164aee38
jevin <jevin@quickjack.ca>
2022-07-02 08:38:45 -0400

    Testing out sunpaper home-manager setup

--- flake.nix
+++ flake.nix
@@ -3,85 +3,46 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager/release-22.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "/home/jevin/code/personal/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
     nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = inputs@{ home-manager, nix-colors, nixpkgs-unstable,... }: {
+  outputs = { nixpkgs, home-manager, nix-colors, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      homeConfigurations.jevin = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-      packages.x86_64-linux.homeConfigurations.jevin = home-manager.lib.homeManagerConfiguration {
-        # Specify the path to your home configuration here
-        configuration =  { pkgs, ... }:
-          let
-            overlay-unstable = final: prev: {
-              unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
-            };
-          in
+        # nixpkgs.config.allowUnfree = true;
+
+        modules = [
+          ./home.nix
+          ./vim.nix
+          ./zsh.nix
+          ./cli-common.nix
+          ./cli-linux.nix
+          ./desktop-linux-personal.nix
+          ./mutt-quickjack.nix
+          # ./amateur_radio.nix
+          ./theme-personal.nix
           {
-            nixpkgs.overlays = [ overlay-unstable ];
-            nixpkgs.config.allowUnfree = true;
-            imports = [
-              ./home.nix
-              ./vim.nix
-              ./zsh.nix
-              ./cli-common.nix
-              ./cli-linux.nix
-              ./desktop-linux-personal.nix
-              ./mutt-quickjack.nix
-              # ./amateur_radio.nix
-              ./theme-personal.nix
-            ];
-          };
+            home = {
+              username = "jevin";
+              homeDirectory = "/home/jevin";
+              stateVersion = "22.05";
+            };
+          }
+        ];
 
         extraSpecialArgs = { inherit nix-colors; };
-        system = "x86_64-linux";
-        username = "jevin";
-        homeDirectory = "/home/jevin";
-        stateVersion = "22.05";
-      };
-
-      packages.x86_64-linux.homeConfigurations.jevinhumi = home-manager.lib.homeManagerConfiguration {
-        # Specify the path to your home configuration here
-        configuration =  { pkgs, ... }:
-        {
-          imports = [
-           ./home.nix
-           ./vim.nix
-           ./zsh.nix
-           ./cli-common.nix
-           ./cli-linux.nix
-           ./desktop-linux-work.nix
-           ./mutt-humi.nix
-          ];
-        };
-
-        system = "x86_64-linux";
-        username = "jevinhumi";
-        homeDirectory = "/home/jevinhumi";
-        stateVersion = "22.05";
-      };
-
-      packages.aarch64-darwin.homeConfigurations.jevin = home-manager.lib.homeManagerConfiguration {
-        # Specify the path to your home configuration here
-        configuration =  { pkgs, ... }:
-        {
-          imports = [
-           ./home.nix
-           ./vim.nix
-           ./zsh.nix
-           ./cli-common.nix
-           ./desktop-mac.nix
-           # ./mutt-humi.nix # No darwin for lieer
-          ];
-        };
-
-        system = "aarch64-darwin";
-        username = "jevin";
-        homeDirectory = "/Users/jevin";
-        stateVersion = "21.11";
       };
     };
+
 }
