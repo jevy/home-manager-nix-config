9159e03e34191fcc96ce0670849225517a7b9489
jevin <jevin@quickjack.ca>
2022-08-13 13:56:52 -0400

    Screenlock and display working properly

--- desktop-linux-common.nix
+++ desktop-linux-common.nix
@@ -65,16 +65,15 @@
   };
 
   services.swayidle = let
-    lock_command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2" ;
+    lock_command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2" ;
   in { enable = true;
        events = [
          { event = "before-sleep"; command = lock_command; }
-         # { event = "after-resume"; command = "swaymsg 'output * dpms off'"; }
-         { event = "lock"; command = "lock"; }
+         # { event = "lock"; command = "lock"; }
        ];
     timeouts = [
-      { timeout = 120; command = lock_command; }
-      # { timeout = 240; command = "swaymsg 'output * dpms on'"; }
+      { timeout = 60; command = lock_command; }
+      { timeout = 90; command = "swaymsg 'output * dpms off'"; resumeCommand = "swaymsg 'output * dpms on'"; }
     ];
   };
 
