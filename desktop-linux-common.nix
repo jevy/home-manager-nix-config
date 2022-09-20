5406206b037aab08d9b5019c490fce03c003aa0f
Jevin Maltais <jevin.maltais@humi.ca>
2022-09-20 09:31:51 -0400

    Correct next/prev key bindings

--- desktop-linux-common.nix
+++ desktop-linux-common.nix
@@ -198,6 +198,8 @@
           "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 10";
           "XF86AudioRaiseVolume" = "exec ${pkgs.pamixer}/bin/pamixer -i 10";
           "Pause"                = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          "XF86AudioNext"        = "exec ${pkgs.playerctl}/bin/playerctl next";
+          "XF86AudioPrev"        = "exec ${pkgs.playerctl}/bin/playerctl previous";
           "Print"                = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp)\" - | ${pkgs.swappy}/bin/swappy -f -";
           "${modifier}+n"        = "exec ${pkgs.mako}/bin/makoctl dismiss";
         };
