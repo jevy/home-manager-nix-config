d292b83661444bd03b1a3f65029044db65ca41e7
jevin <jevin@quickjack.ca>
2022-11-13 08:38:03 -0500

    Trying to make it host specific

--- flake.nix
+++ flake.nix
@@ -11,46 +11,37 @@
     nix-colors.url                      = "github:misterio77/nix-colors";
   };
   outputs = { home-manager, nix-colors, nixpkgs, nixpkgs-unstable, nixos-hardware, ... }@inputs: {
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    # Modeling it after: https://rycee.gitlab.io/home-manager/index.html#sec-flakes-nixos-module
     nixosConfigurations = {
+
       # Lenovo has hostname `nixos`
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux;
         specialArgs = { inherit inputs; }; # Pass flake inputs to our config
         modules = [
           ./configuration.nix
           ./hardware-configuration.nix
           nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.jevin = {
+              imports = [ ./jevin-linux.nix ];
+            };
+          }
+          {
+            home-manager.users.jevinhumi = {
+              imports = [ ./work-linux.nix ];
+            };
+          }
         ];
+        home-manager.extraSpecialArgs = { inherit nix-colors; };
       };
+
+      # Other laptop goes here. Either Linux or Mac
+
     };
-
-    homeConfigurations =
-    let
-      system = "x86_64-linux";
-      # pkgs = import nixpkgs {
-      #   inherit system;
-      # };
-    in
-    {
-      "jevin" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; inherit nix-colors; }; # Pass flake inputs to our config
-        modules = [ ./jevin-linux.nix ];
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      };
-
-      "jevinhumi" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; inherit nix-colors; }; # Pass flake inputs to our config
-        modules = [ ./work-linux.nix ];
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      };
-    };
-
 
   };
 }
