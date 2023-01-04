6addf8b9e8be995c6615ddacb6cfc75aad1640a9
Jevin Maltais <jevin@quickjack.ca>
2023-01-03 23:17:19 -0500

    Fixing screen

--- desktop-linux-common.nix
+++ desktop-linux-common.nix
@@ -114,15 +114,12 @@
       menu = "rofi -show run";
 
       startup =
-        let
-          lock_command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2";
-        in
         [
         { command = "${pkgs.slack}/bin/slack"; }
         { command = "spotify"; }
         { command = "${pkgs.flashfocus}/bin/flashfocus"; }
         { command = "${pkgs.unstable._1password-gui}/bin/1password"; }
-        { command = "swayidle -w timeout 300 'swaylock -f -c 000000' timeout 600 'swaymsg \"output * dpms off\"' resume 'swaymsg \"output * dpms on\"' before-sleep 'swaylock -f -c 000000'" ;}
+        { command = "swayidle -w timeout 300 'swaylock -f -c 00a00a' timeout 600 'swaymsg \"output * dpms off\"' resume 'swaymsg \"output * dpms on\"' before-sleep 'swaylock -f -c 000000'" ;}
         ];
 
       terminal = "kitty";
