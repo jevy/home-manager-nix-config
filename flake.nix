abb82260cc8790fbbe50e5b6f24bd19504beb6a3
Jevin Maltais <jevin@quickjack.ca>
2023-10-08 08:28:15 -0400

    Removing Darwin config

--- flake.nix
+++ flake.nix
@@ -8,19 +8,18 @@
     nixpkgs-unstable.url                = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url                          = "github:danth/stylix/7bcf3ce6c9e9225e87d4e3b0c2e7d27a39954c02";
+    hyprland.url                        = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, home-manager, stylix, nixpkgs, nixpkgs-unstable, nixos-hardware, ... }@inputs:
+  outputs = { self, home-manager, stylix, nixpkgs, nixpkgs-unstable, nixos-hardware, hyprland, ... }@inputs:
 
     let
-      # Define a function that creates a system configuration
       mkSystemConfiguration = system: modules: nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
         modules = modules;
       };
 
-      # Define the custom overlay
       custom-overlays = final: prev: {
         unstable = import nixpkgs-unstable {
           system = "x86_64-linux";
@@ -28,7 +27,6 @@
         };
       };
 
-      # Define the list of modules used for the Linux system
       linuxModules = [
         ({ config, pkgs, ... }: { nixpkgs.overlays = [ custom-overlays ]; })
         ./nixos/configuration.nix
@@ -49,27 +47,10 @@
         }
       ];
 
-      # Define the Home Manager configuration for the Darwin system
-      jevinDarwin = {
-        # You would replace this with your Darwin-specific Home Manager configuration
-        imports = [ ./jevin-darwin.nix ];
-        home.username = "jevin";
-        home.homeDirectory = "/Users/jevin";
-      };
-
     in
     {
       nixosConfigurations = {
         x86_64-linux = mkSystemConfiguration "x86_64-linux" linuxModules;
-      };
-      homeConfigurations = {
-        jevin-darwin = home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-darwin";
-          username = "jevin";
-          homeDirectory = "/Users/jevin";
-          configuration = jevinDarwin;
-          extraSpecialArgs = inputs;
-        };
       };
     };
 }
