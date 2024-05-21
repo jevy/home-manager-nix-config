65037c83154db44509a8bc4107b5bb059d936579
jevin <jevin@quickjack.ca>
2024-05-21 14:36:27 -0400

    Cleaning up themes

--- theme-mac.nix
+++ theme-mac.nix
@@ -2,15 +2,8 @@
   pkgs,
   stylix,
   ...
-}: let
-  base16-schemes = pkgs.fetchFromGitHub {
-    owner = "tinted-theming";
-    repo = "base16-schemes";
-    rev = "cf6bc892a24af19e11383adedc6ce7901f133ea7";
-    sha256 = "sha256-U9pfie3qABp5sTr3M9ga/jX8C807FeiXlmEZnC4ZM58=";
-  };
-in {
-  stylix.base16Scheme = "${base16-schemes}/gruvbox-material-dark-soft.yaml";
+}: {
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-light-soft.yaml";
 
   stylix.autoEnable = false;
   stylix.targets.tmux.enable = true;
