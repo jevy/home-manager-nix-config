6e4c728c51b90007e6117a276951c3c79416892f
jevin <jevin@quickjack.ca>
2024-09-18 08:29:45 -0400

    Show pomodoro in my work shell

--- zsh-clio.nix
+++ zsh-clio.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, libs, ... }:
+{ pkgs, ... }:
 
 {
   programs.zsh = {
     initExtra = ''
       eval "$(dev _hook)"
     '';
+  };
+  home.shellAliases = {
+    pomodoro = "${pkgs.termdown}/bin/termdown 25m -s -b";
   };
 }
 
