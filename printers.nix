343eacffdc9271f84c321768175a5110c71290fe
Jevin Maltais <jevin@quickjack.ca>
2024-09-27 15:55:46 -0400

    New printer IP

--- printers.nix
+++ printers.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, ... }:
 {
-
+  config,
+  pkgs,
+  ...
+}: {
   # Enable CUPS to print documents.
   services.printing = {
     enable = true;
-    drivers = [ pkgs.hplip ];
+    drivers = [pkgs.hplip];
   };
 
   # Jevin - If need to regenerate the deviceUri: To add the printer; 1. `nix-shell -p hplip` 2. hp-makeuri <IP> 3. Add that URL to cups
@@ -12,11 +14,9 @@
   hardware.printers.ensurePrinters = [
     {
       model = "drv:///hp/hpcups.drv/hp-officejet_pro_9010_series.ppd";
-      deviceUri = "hp:/net/HP_OfficeJet_Pro_9010_series?ip=192.168.1.85";
+      deviceUri = "hp:/net/HP_OfficeJet_Pro_9010_series?ip=192.168.1.76";
       location = "Basement";
       name = "HP_Officejet_Pro_9010";
-
     }
   ];
-
 }
