8fb8d856be3440b49dcc20d8cb8ba00b7854d402
jevin <jevin@quickjack.ca>
2024-10-07 11:23:42 -0400

    Getting setup with spellbook repo

--- zsh-spellbook.nix
+++ zsh-spellbook.nix
@@ -1,4 +1,9 @@
 {pkgs, ...}: {
+  programs.zsh = {
+    initExtra = ''
+      export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+    '';
+  };
   home.shellAliases = {
     pomodoro = "${pkgs.termdown}/bin/termdown 25m -s -b";
   };
