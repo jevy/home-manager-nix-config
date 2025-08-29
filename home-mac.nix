3c15a6f889816436cac571e0e7efa271212284d6
jevin <jevin@quickjack.ca>
2025-08-29 12:33:30 -0400

    Adding go into path

--- home-mac.nix
+++ home-mac.nix
@@ -1,40 +1,24 @@
+{ config, pkgs, inputs, ... }:
 {
-  config,
-  pkgs,
-  ...
-}: {
-  # nixpkgs.config.allowUnfreePredicate = (pkg: true);
+  imports = [
+    ./stylix-common.nix
+    inputs.spicetify-nix.homeManagerModules.spicetify
+  ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "23.11";
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.git = {
     enable = true;
     userName = "jevin";
     userEmail = "jevin@quickjack.ca";
-    aliases = {
-      st = "status";
-    };
+    aliases = { st = "status"; };
     difftastic.enable = true;
   };
 
   home.keyboard = {
     layout = "us";
     variant = "qwerty";
-    options = ["ctrl:nocaps"];
+    options = [ "ctrl:nocaps" ];
   };
 }
-  imports = [
-    ./stylix-common.nix
-    inputs.spicetify-nix.homeManagerModules.spicetify
-  ];
--- zsh-spellbook.nix
+++ zsh-spellbook.nix
@@ -1,7 +1,8 @@
 {pkgs, ...}: {
   programs.zsh = {
-    initExtra = ''
+    initContent = ''
       export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+      [[ :$PATH: == *":$HOME/go/bin:"* ]] || PATH+=":$HOME/go/bin"; export PATH
       export LOCAL_BIN="$HOME/.local/bin"; [[ :$PATH: == *":$LOCAL_BIN:"* ]] || PATH+=":$LOCAL_BIN"
       if [[ -f /Users/jevin/secrets/node_auth ]]; then
         export NODE_AUTH_TOKEN=$(< /Users/jevin/secrets/node_auth)
