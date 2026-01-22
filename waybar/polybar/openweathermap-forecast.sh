30d522f7e692446f5d05c747e36bf98ffc75053e
Jevin Maltais <jevin@quickjack.ca>
2026-01-22 18:55:44 -0500

    Correcting the Weather Icons for ashell

--- waybar/polybar/openweathermap-forecast.sh
+++ waybar/polybar/openweathermap-forecast.sh
@@ -2,40 +2,20 @@
 
 get_icon() {
     case $1 in
-        # Icons for weather-icons
-        01d) icon="";;
-        01n) icon="";;
-        02d) icon="";;
-        02n) icon="";;
-        03*) icon="";;
-        04*) icon="";;
-        09d) icon="";;
-        09n) icon="";;
-        10d) icon="";;
-        10n) icon="";;
-        11d) icon="";;
-        11n) icon="";;
-        13d) icon="";;
-        13n) icon="";;
-        50d) icon="";;
-        50n) icon="";;
-        *) icon="";
-
-        # Icons for Font Awesome 5 Pro
-        #01d) icon="";;
-        #01n) icon="";;
-        #02d) icon="";;
-        #02n) icon="";;
-        #03d) icon="";;
-        #03n) icon="";;
-        #04*) icon="";;
-        #09*) icon="";;
-        #10d) icon="";;
-        #10n) icon="";;
-        #11*) icon="";;
-        #13*) icon="";;
-        #50*) icon="";;
-        #*) icon="";
+        # Nerd Font weather icons (Material Design)
+        01d) icon="󰖙";;  # sunny
+        01n) icon="󰖔";;  # night clear
+        02d) icon="󰖕";;  # partly cloudy day
+        02n) icon="󰼱";;  # partly cloudy night
+        03*) icon="󰖐";;  # cloudy
+        04*) icon="󰖐";;  # broken clouds
+        09*) icon="󰖖";;  # shower rain
+        10d) icon="󰖗";;  # rain day
+        10n) icon="󰖗";;  # rain night
+        11*) icon="󰙾";;  # thunderstorm
+        13*) icon="󰼶";;  # snow
+        50*) icon="󰖑";;  # fog/mist
+        *) icon="󰖐";;    # default cloudy
     esac
 
     echo $icon
@@ -79,11 +59,11 @@ if [ -n "$current" ] && [ -n "$forecast" ]; then
     forecast_icon=$(echo "$forecast" | jq -r ".list[].weather[0].icon")
 
     if [ "$current_temp" -gt "$forecast_temp" ]; then
-        trend=""
+        trend=""
     elif [ "$forecast_temp" -gt "$current_temp" ]; then
-        trend=""
+        trend=""
     else
-        trend=""
+        trend=""
     fi
 
     echo "$current_location $(get_icon "$current_icon") $current_temp$SYMBOL  $trend  $(get_icon "$forecast_icon") $forecast_temp$SYMBOL"
