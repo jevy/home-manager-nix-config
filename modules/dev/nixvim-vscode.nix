3372adda679f71ad4d1f7a15b5f04f746f6c4de3
Jevin Maltais <jevin@quickjack.ca>
2026-02-09 19:09:59 -0500

    Inline nixvim-vscode.nix config into its module
    Move the VSCode neovim configuration from a standalone root-level
    data file into a let binding in modules/dev/nixvim-vscode.nix,
    eliminating the extra file.

--- modules/dev/nixvim-vscode.nix
+++ modules/dev/nixvim-vscode.nix
@@ -1,17 +1,69 @@
 # VSCode Neovim init file (extracts init-vscode.lua from nixvim config)
 { inputs, ... }:
+let
+  nixvimVscodeConfig = {
+    opts = {
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      splitbelow = true;
+      showmatch = true;
+      ignorecase = true;
+      scrolloff = 5;
+      incsearch = true;
+      writebackup = false;
+      backup = false;
+    };
+
+    plugins = {
+      leap.enable = true;
+
+      treesitter = {
+        enable = false;
+        settings = {
+          indent.enable = true;
+          highlight.enable = false;
+        };
+      };
+
+      lsp.enable = false;
+      lsp-format.enable = false;
+    };
+
+    extraConfigLua = ''
+      if not vim.g.vscode then
+        vim.o.hlsearch = false
+        return
+      end
+
+      vim.opt.number = false
+      vim.opt.relativenumber = false
+
+      local map = vim.keymap.set
+      local ok_vscode, vscode = pcall(require, 'vscode')
+      if ok_vscode then
+        map('n', '<C-o>', function()
+          vscode.action('workbench.action.navigateBack')
+        end, { noremap = true, silent = true })
+        map('n', '<C-i>', function()
+          vscode.action('workbench.action.navigateForward')
+        end, { noremap = true, silent = true })
+      end
+    '';
+  };
+in
 {
   # Standalone nvim-vscode package (available on all configured systems)
   perSystem = { system, ... }: {
     packages.nvim-vscode =
-      inputs.nixvim.legacyPackages.${system}.makeNixvim (import ../../nixvim-vscode.nix);
+      inputs.nixvim.legacyPackages.${system}.makeNixvim nixvimVscodeConfig;
   };
 
   flake.modules.homeManager.nixvimVscode =
     { pkgs, ... }:
     let
       nixvimPkgs = inputs.nixvim.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-      nvimVscode = nixvimPkgs.makeNixvim (import ../../nixvim-vscode.nix);
+      nvimVscode = nixvimPkgs.makeNixvim nixvimVscodeConfig;
       nvimWrapperText = builtins.readFile "${nvimVscode}/bin/nvim";
       parts = pkgs.lib.strings.splitString " -u " nvimWrapperText;
       initVscodePath =
