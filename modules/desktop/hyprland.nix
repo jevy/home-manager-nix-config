062595755ff260d5aed76601f221e9d8884def73
jevin <jevin@quickjack.ca>
2026-03-08 15:38:03 -0400

    Screen res after lock

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -497,7 +497,7 @@
           general = {
             lock_cmd = "pidof hyprlock || hyprlock";
             before_sleep_cmd = "loginctl lock-session";
-            after_sleep_cmd = "hyprctl dispatch dpms on";
+            after_sleep_cmd = "hyprctl dispatch dpms on && sleep 1 && hyprctl reload";
           };
           listener = [
             {
