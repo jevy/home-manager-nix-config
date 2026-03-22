30d99a35340eb00e5ab12639bb08cc3ad85787fe
jevin <jevin@quickjack.ca>
2026-03-22 08:15:52 -0400

    Update neomutt-for-gmail and move goobook config upstream

--- modules/apps/mutt.nix
+++ modules/apps/mutt.nix
@@ -19,16 +19,14 @@
 
       };
 
-      # Personal macros (module provides Gmail defaults)
+      # Personal macros (module provides Gmail + goobook defaults)
       programs.neomutt.macros = [
         { map = ["index" "pager"]; key = "b"; action = "<resend-message>"; }
-        { map = ["index" "pager"]; key = "a"; action = "<pipe-message>goobook add<return>"; }
       ];
 
       # Personal overrides on top of module defaults
       programs.neomutt.extraConfig = ''
         set use_threads=threads sort=reverse-last-date sort_aux=date
-        set query_command="goobook query %s"
         set index_format='%4C %Z %<[y?%<[m?%<[d?%[%l:%M%p ]&%[%a %d ]>&%[%b %d ]>&%[%m/%y ]> %-15.15L  %s %g'
         set sidebar_format = "%D%* %n"
         set mailcap_path = ~/.config/mailcap
