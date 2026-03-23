41570aca9b32464a4563ac7b099d7baf760bf3ea
jevin <jevin@quickjack.ca>
2026-03-23 08:29:23 -0400

    Move HTML rendering and urlscan config upstream to neomutt-for-gmail
    w3m mailcap, alternative_order, urlscan config, and mailcap_path
    are now provided by the upstream module. Remove local overrides.

--- modules/apps/mutt.nix
+++ modules/apps/mutt.nix
@@ -24,16 +24,6 @@
         { map = ["index" "pager"]; key = "b"; action = "<resend-message>"; }
       ];
 
-      home.packages = [ pkgs.w3m ];
-
-      xdg.configFile."mailcap".text = ''
-        text/html; w3m -dump -T text/html -cols 80 -o display_link_number=1; copiousoutput
-        application/*; xdg-open %s &; test=test -n "$DISPLAY"
-        image/*; xdg-open %s &; test=test -n "$DISPLAY"
-        video/*; xdg-open %s &; test=test -n "$DISPLAY"
-        audio/*; xdg-open %s &; test=test -n "$DISPLAY"
-      '';
-
       # Personal overrides on top of module defaults
       programs.neomutt.extraConfig =
         builtins.readFile ./gruvbox.neomuttrc
@@ -41,9 +31,7 @@
           set use_threads=threads sort=reverse-last-date sort_aux=date
           set index_format='%4C %Z %<[y?%<[m?%<[d?%[%l:%M%p ]&%[%a %d ]>&%[%b %d ]>&%[%m/%y ]> %-15.15L  %s %g'
           set sidebar_format = "%D%* %n"
-          set mailcap_path = ~/.config/mailcap
           auto_view application/pdf application/vnd.openxmlformats-officedocument.wordprocessingml.document
-          alternative_order text/enriched text/html text/plain
         '';
     };
 }
